1ea2e8e6f9c145caf69c6035eea91a7229a8b439
jurgen.de.landsheer <jurgen.de.landsheer@gmail.com>
2023-03-22 21:18:20 +0100

    bootstrap-angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MaterialModule} from "./material/material.module";
+import { MainContentComponent } from './component/main-content/main-content.component';
 
 @NgModule({
 	declarations: [
-		AppComponent
+		AppComponent,
+  MainContentComponent
 	],
 	imports: [
 		BrowserModule,
